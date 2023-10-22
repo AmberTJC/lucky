94c52203c9ae1792bf39bd5b88c6e3a64a0cbfea
Amber <amber.campbell@edu.sait.ca>
2023-10-22 12:20:59 -0600

    Letter count

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 with open("raw.githubusercontent.com_catnap-amuck_august_main_mydefaults.ini.txt") as ini_file:
     data = ini_file.read()
+
+letters = len( [letter for letter in data if letter.isalpha])
+print (letters)
