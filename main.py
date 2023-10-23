835e8a6e046ba3feba2d77efb32cb7c2fffb8426
Amber <amber.campbell@edu.sait.ca>
2023-10-23 11:23:05 -0600

    Number count

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ with open("raw.githubusercontent.com_catnap-amuck_august_main_mydefaults.ini.txt
     data = ini_file.read()
 
 letters = 0
+numbers = 0
 for alpha in data:
     if (alpha.isalpha()):
         letters += 1
 print (f"the number of letters is {letters}")
+
+for num in data:
+    if (num.isdigit()):
+        numbers += 1
+print (f"The number of digits is {numbers}")
