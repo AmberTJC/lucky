e7ca9532134edc898e96ddc17fecabbacde44116
Amber <amber.campbell@edu.sait.ca>
2023-10-23 11:19:42 -0600

    letters working

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 with open("raw.githubusercontent.com_catnap-amuck_august_main_mydefaults.ini.txt") as ini_file:
     data = ini_file.read()
 
-letters = len( [letter for letter in data if letter.isalpha])
-print (letters)
+letters = 0
+for alpha in data:
+    if (alpha.isalpha()):
+        letters += 1
+print (f"the number of letters is {letters}")
